5277528fc90f49247a2e84d44cdf7e1e48610aa0
Joshua Book <joshua.book@gmail.com>
2016-05-20 11:12:25 -0400

    add sass stuff

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './styles.scss';
+
 export default class App extends React.Component {
   render () {
     return (
-      <div>
+      <div id='app'>
         <h1>react webpack babel es2015 mocha hmre boilerplate</h1>
         <p>by booivus</p>
       </div>
