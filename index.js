7392e35b907a1dd9582c8dd60110e697d437941c
Joshua Book <joshua.book@gmail.com>
2016-05-20 10:50:08 -0400

    initial commit - server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('container')
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
